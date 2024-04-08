267ba7565acef5a87b086e72121a0b9807d476ac
Arash Bordbar <125759470+arashbrd@users.noreply.github.com>
2024-04-08 13:33:01 +0330

    Update Test_sms.py
    delete my phone number

--- Test_sms.py
+++ Test_sms.py
@@ -2,7 +2,7 @@ from SendSMS import SendSMS
 try:
     #تست ارسال پیامک
     txt='تست ارسال پیام'
-    user=['a','b','09173303491']
+    user=['a','b','091712345678']
     res=SendSMS(user,txt)
     if res['messages'][0]['status']==0:
         print('Success!!!')
